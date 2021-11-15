7aef32d442debaecb5a79d9ae784f2659eb9f315
casa92 <casartelli92@gmail.com>
2021-11-15 14:44:40 +0100

    finito

---
+++ main.js
@@ -0,0 +1,35 @@
+// //METODO 1
+// const hambugerIcon = document.querySelector('i.fas.fa-bars');
+// const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+// hambugerIcon.addEventListener('click', 
+//     function() {
+//         hamburgerMenu.style.display = 'block';
+//     } 
+// );
+
+// const closehamburgerMenu = document.querySelector('.close');
+
+// closehamburgerMenu.addEventListener('click',
+//     function() {
+//         hamburgerMenu.style.display = 'none'
+//     }
+// );
+
+//METODO 2
+const hambugerIcon = document.querySelector('i.fas.fa-bars');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hambugerIcon.addEventListener('click', 
+    function() {
+        hamburgerMenu.classList.add('active');
+    }
+);
+
+const closehamburgerMenu = document.querySelector('.close');
+
+closehamburgerMenu.addEventListener('click',
+    function() {
+        hamburgerMenu.classList.remove('active');
+    }
+);
